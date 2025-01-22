c909277aaf21bc5760e0d0d81c0df3e5a7e80ecc
mauroSoliz98 <maurososoliz@gmail.com>
2025-01-22 18:45:33 -0400

    Alter model and router departure

--- src/models/departure_model.py
+++ src/models/departure_model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pydantic import BaseModel
-
+from src.models.product_model import Product_Reference
 
 # Se crea un modelo Pydantic para representar una tarea individual
 class Departure(BaseModel):
@@ -20,3 +20,8 @@ class Departure_product(BaseModel):
     quantity: int
 
     unit_price: float
+
+class DepartureItem(BaseModel):
+    departure: Departure
+    product: Product_Reference
+    departure_product: Departure_product
--- src/routers/departure_router.py
+++ src/routers/departure_router.py
@@ -1,34 +1,44 @@
+from typing import List
 from fastapi import APIRouter, status, HTTPException
 from prisma import Prisma
-from src.models.departure_model import Departure, Departure_product
-from src.models.product_model import Product_Reference
+from src.models.departure_model import Departure, DepartureItem
+
+
 db = Prisma()
 departureRoute = APIRouter()
+
 
 @departureRoute.get("/")
 async def get_todos():
     await db.connect()
     try:
         # Recupera datos de departure_product incluyendo relaciones
-        data = await db.departure_product.find_many(include={
-            'departures': True,
-            'products': True
-        })
+        data = await db.departure_product.find_many(
+            include={"departures": True, "products": True}
+        )
 
         # Filtra los datos para excluir los IDs
         filtered_data = [
-            {               
-                'departure': {
-                    'id': item.departures.id,
-                    'create_at': item.departures.create_at,
-                    'destiny': item.departures.destiny
-                } if item.departures else None,
-                'product': {
-                    'name': item.products.name,
-                } if item.products else None,
-                'order':item.order,
-                'quantity': item.quantity,
-                'unit_price': item.unit_price
+            {
+                "departure": (
+                    {
+                        "id": item.departures.id,
+                        "create_at": item.departures.create_at,
+                        "destiny": item.departures.destiny,
+                    }
+                    if item.departures
+                    else None
+                ),
+                "product": (
+                    {
+                        "name": item.products.name,
+                    }
+                    if item.products
+                    else None
+                ),
+                "order": item.order,
+                "quantity": item.quantity,
+                "unit_price": item.unit_price,
             }
             for item in data
         ]
@@ -38,40 +48,50 @@ async def get_todos():
         await db.disconnect()
 
 
-
 @departureRoute.get("/{id}")
 async def get_todo(id: int):
     await db.connect()
-    data = await db.departure.find_unique(where={'id':id} )
+    data = await db.departure.find_unique(where={"id": id})
     await db.disconnect()
     return data
 
 
-
 @departureRoute.post("/", status_code=status.HTTP_201_CREATED)
-async def create_todo(departure: Departure, product: Product_Reference, departure_product: Departure_product):
+async def create_todo(items: List[DepartureItem]):
 
-  await db.connect()
-  #Agregamos el nuevo item al stock
-  new_departure = await db.departure.create(
-    data = 
-      {
-        "destiny": departure.destiny,
-        "create_at": departure.create_at,
-        "departure_product": {
-            "create": [
-                {
-                    "id_product": product.id,
-                    "quantity": departure_product.quantity,
-                    "unit_price": departure_product.unit_price,
-                }
-            ]
-        }
-      },   
+    await db.connect()
+
+    # Verificar que todos los productos existen
+    product_ids = [item.product.id for item in items]
+    existing_products = await db.product.find_many(where={"id": {"in": product_ids}})
+    existing_product_ids = {product.id for product in existing_products}
+
+    for item in items:
+        if item.product.id not in existing_product_ids:
+            await db.disconnect()
+            raise HTTPException(status_code=400, detail=f"Product with id {item.product.id} does not exist")
+
+    # Agregamos los nuevos items al stock
+    new_departure = await db.departure.create(
+        data={
+            "destiny": items[0].departure.destiny,
+            "create_at": items[0].departure.create_at,
+            "departure_product": {
+                "create": [
+                    {
+                        "id_product": item.product.id,
+                        "quantity": item.departure_product.quantity,
+                        "unit_price": item.departure_product.unit_price,
+                    }
+                    for item in items
+                ]
+            },
+        },
     )
-  
-  await db.disconnect()
-  return new_departure
+
+    await db.disconnect()
+    return new_departure
+
 
 @departureRoute.delete("/{id}", status_code=status.HTTP_200_OK)
 async def delete_todo(id: int):
@@ -80,10 +100,11 @@ async def delete_todo(id: int):
     await db.disconnect()
     return data
 
+
 @departureRoute.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
 async def update_departure(id: int, departure: Departure):
-  await db.connect()
+    await db.connect()
 
-  data = await db.departure.update(where={'id': id}, data=departure.model_dump())
-  await db.disconnect()
-  return data
+    data = await db.departure.update(where={"id": id}, data=departure.model_dump())
+    await db.disconnect()
+    return data
